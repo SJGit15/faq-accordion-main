2a0e1f1bdc373d2f46dd4c0b0f51af49692375d2
SJGit15 <153937670+SJGit15@users.noreply.github.com>
2024-09-01 17:04:26 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+function reveal() {
+    var x = document.getElementById("show");
+    var y = document.getElementById("change");
+
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+        y.style.filter= "brightness(0%)";
+        y.style.backgroundImage="url(assets/images/icon-minus.svg)";
+    } 
+    else {
+        x.style.display = "none";
+        y.style.filter="none";
+        y.style.backgroundImage="none";
+    }
+}
+
+function reveal2() {
+    var x = document.getElementById("show2");
+    var y = document.getElementById("change2");
+
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+        y.style.filter= "brightness(0%)";
+        y.style.backgroundImage="url(assets/images/icon-minus.svg)";
+    } 
+    else {
+        x.style.display = "none";
+        y.style.filter="none";
+        y.style.backgroundImage="none";
+    }
+}
+
+function reveal3() {
+    var x = document.getElementById("show3");
+    var y = document.getElementById("change3");
+
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+        y.style.filter= "brightness(0%)";
+        y.style.backgroundImage="url(assets/images/icon-minus.svg)";
+    } 
+    else {
+        x.style.display = "none";
+        y.style.filter="none";
+        y.style.backgroundImage="none";
+    }
+}
+
+function reveal4() {
+    var x = document.getElementById("show4");
+    var y = document.getElementById("change4");
+
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+        y.style.filter= "brightness(0%)";
+        y.style.backgroundImage="url(assets/images/icon-minus.svg)";
+    } 
+    else {
+        x.style.display = "none";
+        y.style.filter="none";
+        y.style.backgroundImage="none";
+    }
+}
